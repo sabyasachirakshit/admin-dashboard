1040f41eeeb888e52c7ab197e667f3590c767d71
sabyasachirakshit <sabyasachirakshitacs19@gmail.com>
2023-10-16 13:05:36 +0530

    more stats added

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -12,10 +12,73 @@ function Statistics() {
         alignItems: "center",
       }}
     >
-      <div
-        className="stats-1"
-        style={{ display: "flex", gap: 20, width: "100%" }}
-      >
+      <div className="stats-1" style={{ display: "flex", width: "100%" }}>
+        <div
+          className="st-1"
+          style={{
+            backgroundColor: "white",
+            display: "flex",
+            gap: 3,
+            width: "24%",
+            gap: 3,
+            borderRadius: 8,
+            marginLeft: 18,
+          }}
+        >
+          <img src="" alt="stats" />
+          <div
+            className="details"
+            style={{ display: "flex", flexDirection: "column", gap: 2 }}
+          >
+            <span>Earning</span>
+            <span>$198K</span>
+            <span>37.8% this month</span>
+          </div>
+        </div>
+        <div
+          className="st-1"
+          style={{
+            backgroundColor: "white",
+            display: "flex",
+            gap: 3,
+            width: "24%",
+            gap: 3,
+            borderRadius: 8,
+            marginLeft: 18,
+          }}
+        >
+          <img src="" alt="stats" />
+          <div
+            className="details"
+            style={{ display: "flex", flexDirection: "column", gap: 2 }}
+          >
+            <span>Earning</span>
+            <span>$198K</span>
+            <span>37.8% this month</span>
+          </div>
+        </div>
+        <div
+          className="st-1"
+          style={{
+            backgroundColor: "white",
+            display: "flex",
+            gap: 3,
+            width: "24%",
+            gap: 3,
+            borderRadius: 8,
+            marginLeft: 18,
+          }}
+        >
+          <img src="" alt="stats" />
+          <div
+            className="details"
+            style={{ display: "flex", flexDirection: "column", gap: 2 }}
+          >
+            <span>Earning</span>
+            <span>$198K</span>
+            <span>37.8% this month</span>
+          </div>
+        </div>
         <div
           className="st-1"
           style={{
