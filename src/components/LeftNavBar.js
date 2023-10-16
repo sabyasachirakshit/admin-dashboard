71ec3293607f0480573e90330bb6d0c2275d6d1e
sabyasachirakshit <sabyasachirakshitacs19@gmail.com>
2023-10-16 12:19:33 +0530

    added elements

--- src/components/LeftNavBar.js
+++ src/components/LeftNavBar.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { RadarChartOutlined } from "@ant-design/icons";
+import { RadarChartOutlined, RightOutlined } from "@ant-design/icons";
+import "./leftnav.css";
 export default function LeftNavBar() {
   return (
     <div
@@ -19,6 +20,37 @@ export default function LeftNavBar() {
       >
         <RadarChartOutlined style={{ fontSize: 24, paddingTop: 4 }} />
         <b style={{ fontSize: 24 }}>Dashboard</b>
+      </div>
+
+      <div
+        className="nav-elements"
+        style={{
+          backgroundColor: "red",
+          display: "flex",
+          gap: 1,
+          paddingTop: 35,
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <div className="el">
+          <div style={{ paddingLeft: 11 }}>dashboard</div>
+        </div>
+        <div className="el">
+          <div style={{ paddingLeft: 11 }}>dashboard</div> <RightOutlined />
+        </div>
+        <div className="el">
+          <div style={{ paddingLeft: 11 }}>dashboard</div> <RightOutlined />
+        </div>
+        <div className="el">
+          <div style={{ paddingLeft: 11 }}>dashboard</div> <RightOutlined />
+        </div>
+        <div className="el">
+          <div style={{ paddingLeft: 11 }}>dashboard</div> <RightOutlined />
+        </div>
+        <div className="el">
+          <div style={{ paddingLeft: 11 }}>dashboard</div> <RightOutlined />
+        </div>
       </div>
     </div>
   );
