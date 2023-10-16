94820026bc45a8fc71eb65bc64dd8a9e7522c507
sabyasachirakshit <sabyasachirakshitacs19@gmail.com>
2023-10-16 12:42:32 +0530

    navbar skelet done

--- src/components/LeftNavBar.js
+++ src/components/LeftNavBar.js
@@ -1,5 +1,10 @@
 import React from "react";
-import { RadarChartOutlined, RightOutlined } from "@ant-design/icons";
+import {
+  RadarChartOutlined,
+  RightOutlined,
+  DownOutlined,
+} from "@ant-design/icons";
+import user from "./user.jpg";
 import "./leftnav.css";
 export default function LeftNavBar() {
   return (
@@ -25,7 +30,6 @@ export default function LeftNavBar() {
       <div
         className="nav-elements"
         style={{
-          backgroundColor: "red",
           display: "flex",
           gap: 1,
           paddingTop: 35,
@@ -51,6 +55,44 @@ export default function LeftNavBar() {
         <div className="el">
           <div style={{ paddingLeft: 11 }}>dashboard</div> <RightOutlined />
         </div>
+      </div>
+
+      <div
+        className="nav-footer"
+        style={{
+          display: "flex",
+          backgroundColor: "rgb(45, 19, 139)",
+          marginLeft: 14,
+          marginTop: 450,
+          width: "90%",
+          borderRadius: 4,
+          justifyContent: "space-between",
+        }}
+      >
+        <div
+          className="user-info"
+          style={{
+            display: "flex",
+            paddingLeft: 8,
+            gap: 10,
+            paddingTop: 9,
+            paddingBottom: 12,
+          }}
+        >
+          <img
+            src={user}
+            alt="i"
+            style={{ borderRadius: "50%", width: "32px", height: "32px" }}
+          />
+          <div
+            className="user-name-desg"
+            style={{ display: "flex", flexDirection: "column", gap: 2 }}
+          >
+            <span>Evano</span>
+            <span>Project Manager</span>
+          </div>
+        </div>
+        <DownOutlined style={{ position: "relative", left: -13, top: 20 }} />
       </div>
     </div>
   );
